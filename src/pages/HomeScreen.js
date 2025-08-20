f5e5a57e5057f5c0a42475d2dd95bfb947c7c54f
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2025-08-20 11:19:25 -0300

    Camera funcionando

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Button } from "react-native";
+import {useNavigation} from "@react-navigation/native"
+
+const HomeScreen = () =>{
+
+    const navigation = useNavigation();
+
+    const handleCam = ()=>{
+        navigation.navigate("Cam")
+    }
+
+    return(
+        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+            <Button 
+            title="Abrir Câmera"
+            onPress={handleCam}
+            color="green"
+            />
+        </View>
+    )
+}
+
+export default HomeScreen;
